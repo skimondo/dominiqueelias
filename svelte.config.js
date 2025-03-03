d5e2793765aed15c9eb4c69e3877d5202d68d203
Dominique Elias <elias.dominique@courrier.uqam.ca>
2025-03-03 11:51:18 -0500

    Update amplify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-node';
+// import adapter from '@sveltejs/adapter-node';
+import adapter from 'amplify-adapter';
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
